3a0ba4ecc6fbdecf5d9a6be2828478972f2086dd
SohanSirvi <sirvisohan667@gmail.com>
2025-07-11 20:52:25 +0530

    Add files via upload

---
+++ day03/Ascii.java
@@ -0,0 +1,16 @@
+package day03;
+
+public class Ascii {
+	public static void main(String[]args) {
+		
+		for(char i='A'; i <='Z'; i++ ) {
+			int s =  i;
+			System.out.print(i+" " +s +"  ");
+		}
+		System.out.println("");
+		for(char i='a'; i <='z'; i++ ) {
+			int s =  i;
+			System.out.print(i+" " +s +"  ");
+		}
+	}
+}
---
+++ day03/Dowhileloop.java
@@ -0,0 +1,13 @@
+package day03;
+
+public class Dowhileloop {
+	public static void main(String[]args) {
+		
+		int i = 0;
+		do {
+			System.out.println("Wel-Come");
+			i++;
+		}while(i>5);
+		
+	}
+}
